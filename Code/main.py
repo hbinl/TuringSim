60bd47e51ce7d836ba1a49f36ca1de05e40d4187
Hao Bin <imloh7@gmail.com>
2015-09-22 20:07:12 +0800

    Fixed Zoom

--- Code/main.py
+++ Code/main.py
@@ -1,11 +1,7 @@
 """
-@title FIT3140 Assignment 3 Spikes All in One Version
-@description Spike test, contains Spike 1, 2, 3 and 4
+@title FIT3140 Assignment 5
+@description Sprint 1
 
-            #######################################################
-            To look for codes related to a particular spike,
-            just find "Spike1", "#Spike2", "#Spike3" or "#Spike4"
-            #######################################################
 
 @author Loh Hao Bin, Ashley Ong Yik Mun, Varshinee Devi Servansingh
 @date 1/9/2015
@@ -13,40 +9,12 @@
 
 # Kivy Dependencies
 from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.graphics import *
-from kivy.uix.label import Label
-from kivy.properties import ObjectProperty
-from kivy.uix.button import Button
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.relativelayout import RelativeLayout
-from kivy.factory import Factory
-from kivy.uix.popup import Popup
-from kivy.core.window import Window
-from kivy.graphics import *
-from kivy.uix.label import Label
-from kivy.graphics.transformation import Matrix
-from kivy.animation import Animation
-from kivy.clock import Clock
-
-# Python Libraries
-import os
-import xml.etree.ElementTree as ET
-
-# Team Spike Files
-from ui_lib import *   #Spike2
-from ui_dialog import *
-from savetm_toxml import *   #Spike1
-from machine_graph import SpikeMachine
 from ui_container import Container
 
 
-
-# KIVY HANDLING STUFF BELOW
 class TuringSimApp(App):
     def build(self):
         return Container()
 
-if __name__=="__main__":
+if __name__ == "__main__":
     TuringSimApp().run()
--- Code/ui_container.py
+++ Code/ui_container.py
@@ -52,7 +52,7 @@ class Container(Widget):
         # Disabling single finger move canvas, disabling rotation
         self.zoomed = False
         Window.clearcolor = (0.7, 0.7, 0.7, 1)
-        self.ids.layout_states.scale_max = 1.0
+        self.ids.layout_states.scale_max = 3.05175
         self.ids.layout_states.scale_min = 0.32768
         self.ids.layout_states.translation_touches = 2
         self.ids.layout_states.do_rotation = False
@@ -77,7 +77,7 @@ class Container(Widget):
         # new_ndtm_button = self.home_popup.content.ids.home_new_ndtm
 
         load_button.bind(on_release=self.load_handler)
-        # new_dtm_button.bind(on_release=self.home_popup.dismiss)
+        new_dtm_button.bind(on_release=self.home_popup.dismiss)
 
         print "home"
         # # Assign handler on dismiss
@@ -86,6 +86,117 @@ class Container(Widget):
         # Reveal dialog
         Clock.schedule_once(self.home_popup.open, 0.5)
         #self.home_popup.open()
+
+
+    def load_handler(self, text):
+        # Purpose: Shows a file browser dialog for user to pick an XML file
+
+        self._popup = Popup(title="Load a Turing Machine XML File",
+                            content=BrowseFileChooserWindow(),
+                            size_hint=(0.85, 0.85))
+
+        # Assign handler on dismiss
+        self._popup.bind(on_dismiss=self.after_load_handler)
+        self._popup.content.ids.cancel_button.bind(on_release=self.home_screen)
+
+        # Reveal dialog
+        self._popup.open()
+
+    def after_load_handler(self, obj):
+        # Purpose: To handle loading file and data from XML file
+
+        # Check if the user has selected a file to load from
+        if obj.content.path is not None:
+            self.home_popup.dismiss()
+
+            # Initialising UI variables, clearing canvas and setting the stage
+            self.zoom_reset()
+            self.edit_mode = False
+            board = self.ids.layout_states
+            board.pos = (0,0)
+            board.clear_widgets()
+
+            # Initialising appearance variables for States
+            i = 0
+            win_x, win_y = Window.size
+            start = 150
+            state_size = 100
+            padding = 80
+            y_hint = 0.2
+            state_size = 100
+
+            # reading from XML file
+            xml_tree = ET.parse(obj.content.path)
+            self.xml_tree = xml_tree
+            xml_states = xml_tree.find("states")
+
+            # Start looping through the XML file
+            for child in xml_states:
+                # Calculate position for the state to appear in
+                x_pos = start+(i*(state_size+padding))
+                if x_pos > Window.size[0]:
+                    i = 0
+                    x_pos = start+(i*(state_size+padding))
+                    y_hint += 0.1
+                y_pos = win_y*y_hint
+
+                # If halt, create halt object, else create ordinary state object
+                if child.attrib["name"] == "halt":
+                    state = UIObj_State_Halting(id=str(child.attrib["name"]),
+                                                x = x_pos,
+                                                y = y_pos,
+                                                size=(state_size,state_size))
+                else:
+                    state = UIObj_State(id=str(child.attrib["name"]),
+                                        x = x_pos,
+                                        y = y_pos,
+                                        size=(state_size,state_size))
+
+                # Add the states to the canvas, and keep track of states in memory
+                self.machine.add_state(str(child.attrib["name"]),state)
+                board.add_widget(state)
+                i += 1
+
+                # # TODO_FUTURE TRANSITION STUFFS
+                # transition = UIObj_Transition(tid=str(child.attrib["name"]),
+                #                               origin_x=x_pos+(state_size/2),origin_y=y_pos+(state_size/2),
+                #                               end_x=x_pos*2, end_y=y_pos*2,
+                #                               width=transition_width,
+                #                               curve_factor=25,
+                #                               is_self_state=False)
+                # board.add_widget(transition)
+                # print self.machine.states
+
+
+
+    def close_handler(self):
+        content = BoxLayout(orientation="vertical")
+
+        content.add_widget(Label(text="Are you sure you want to close the TM?"))
+
+        content_box = BoxLayout(orientation="horizontal")
+
+        ok_button = Button(text="Yes")
+        content_box.add_widget(ok_button)
+
+        cancel_button = Button(text="No")
+        content_box.add_widget(cancel_button)
+
+        content.add_widget(content_box)
+
+        self._popup = Popup(title="Return to Home",
+                            content=content,
+                            size_hint=(0.6, 0.3),
+                            auto_dismiss=False)
+
+        ok_button.bind(on_release=self.home_screen)
+        cancel_button.bind(on_release=self._popup.dismiss)
+
+        if self.edit_mode is True:
+            self.edit_mode_controller(False)
+
+        # Reveal dialog
+        self._popup.open()
 
 
     #Spike1
@@ -202,126 +313,26 @@ class Container(Widget):
 
 
 
-    def zoom_handler(self):
-        if self.zoom_counter < 5:
-            self.zoom_counter += 1
-            mat = Matrix().scale(0.8,0.8,0.8)
-            self.ids.layout_states.apply_transform(mat)
-        else:
-            self.zoom_reset()
+    def zoom_handler(self, text):
+        if text == "Zoom In (+)":
+            if self.ids.layout_states.scale < 3.05175:
+                mat = Matrix().scale(1.25,1.25,1.25)
+                self.ids.layout_states.apply_transform(mat)
+        elif text == "Zoom Out (-)":
+            if self.ids.layout_states.scale > 0.32768:
+                mat = Matrix().scale(0.8,0.8,0.8)
+                self.ids.layout_states.apply_transform(mat)
+
+
 
     def zoom_reset(self):
+        self.ids.layout_states.pos = [0,0]
         self.zoom_counter = 0
         factor = 1/self.ids.layout_states.scale
         mat = Matrix().scale(factor, factor, factor)
         self.ids.layout_states.apply_transform(mat)
 
-    def close_handler(self):
-        content = BoxLayout(orientation="vertical")
-
-        content.add_widget(Label(text="Are you sure you want to close the TM?"))
-
-        content_box = BoxLayout(orientation="horizontal")
-
-        ok_button = Button(text="Yes")
-        content_box.add_widget(ok_button)
-
-        cancel_button = Button(text="No")
-        content_box.add_widget(cancel_button)
-
-        content.add_widget(content_box)
-
-        self._popup = Popup(title="Return to Home",
-                            content=content,
-                            size_hint=(0.6, 0.3),
-                            auto_dismiss=False)
-
-        ok_button.bind(on_release=self.home_screen)
-        cancel_button.bind(on_release=self._popup.dismiss)
-
-        if self.edit_mode is True:
-            self.edit_mode_controller(False)
-
-        # Reveal dialog
-        self._popup.open()
 
 
-    def load_handler(self, text):
-        # Purpose: Shows a file browser dialog for user to pick an XML file
 
-        self._popup = Popup(title="Load a Turing Machine XML File",
-                            content=BrowseFileChooserWindow(),
-                            size_hint=(0.85, 0.85))
-
-        # Assign handler on dismiss
-        self._popup.bind(on_dismiss=self.after_load_handler)
-        self._popup.content.ids.cancel_button.bind(on_release=self.home_screen)
-
-        # Reveal dialog
-        self._popup.open()
-
-    def after_load_handler(self, obj):
-        # Purpose: To handle loading file and data from XML file
-
-        # Check if the user has selected a file to load from
-        if obj.content.path is not None:
-            self.home_popup.dismiss()
-
-            # Initialising UI variables, clearing canvas and setting the stage
-            self.zoom_reset()
-            self.edit_mode = False
-            board = self.ids.layout_states
-            board.pos = (0,0)
-            board.clear_widgets()
-
-            # Initialising appearance variables for States
-            i = 0
-            win_x, win_y = Window.size
-            start = 150
-            state_size = 100
-            padding = 80
-            y_hint = 0.2
-            state_size = 100
-
-            # reading from XML file
-            xml_tree = ET.parse(obj.content.path)
-            self.xml_tree = xml_tree
-            xml_states = xml_tree.find("states")
-
-            # Start looping through the XML file
-            for child in xml_states:
-                # Calculate position for the state to appear in
-                x_pos = start+(i*(state_size+padding))
-                if x_pos > Window.size[0]:
-                    i = 0
-                    x_pos = start+(i*(state_size+padding))
-                    y_hint += 0.1
-                y_pos = win_y*y_hint
-
-                # If halt, create halt object, else create ordinary state object
-                if child.attrib["name"] == "halt":
-                    state = UIObj_State_Halting(id=str(child.attrib["name"]),
-                                                x = x_pos,
-                                                y = y_pos,
-                                                size=(state_size,state_size))
-                else:
-                    state = UIObj_State(id=str(child.attrib["name"]),
-                                        x = x_pos,
-                                        y = y_pos,
-                                        size=(state_size,state_size))
-
-                # Add the states to the canvas, and keep track of states in memory
-                self.machine.add_state(str(child.attrib["name"]),state)
-                board.add_widget(state)
-                i += 1
-
-                # # TODO_FUTURE TRANSITION STUFFS
-                # transition = UIObj_Transition(tid=str(child.attrib["name"]),
-                #                               origin_x=x_pos+(state_size/2),origin_y=y_pos+(state_size/2),
-                #                               end_x=x_pos*2, end_y=y_pos*2,
-                #                               width=transition_width,
-                #                               curve_factor=25,
-                #                               is_self_state=False)
-                # board.add_widget(transition)
-                # print self.machine.states
 
